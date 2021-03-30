c88d441d6d43efe6b67ccbf350843ef350d27808
lvera182 <lvera182@gmail.com>
2021-03-29 19:16:16 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+// debugger;
+// let nombre;
+// let edad;
+// nombre = prompt("Ingrese su Nombre");
+// edad = prompt("Ingrese su edad");
+// console.log(nombre);
+// console.log(typeof(nombre));
+// console.log(edad);
+// console.log(typeof(edad));
+
+// let saludar = "Miguel Angel";
+
+function saludar(nombre){
+    console.log("Hola "+nombre);
+}
+
+saludar("leonel");
+saludar("Martha");
+saludar("Juan");
+
+let Saludar;
+Saludar = "Leonel";
+console.warn("Hola "+Saludar);
+Saludar = "Martha";
+console.warn("Hola "+Saludar);
+Saludar = "Juan";
+console.warn("Hola "+Saludar);
